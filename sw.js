cfd6758059d8bbeb8315cb4d9a6c589c3c446cf3
josephkamaukm58-arch <josephkamaukm58@gmail.com>
2026-09-12 18:33:00 +0000

    Add service worker for app support

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "aviator-analyzer-v1";
+
+const FILES = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
